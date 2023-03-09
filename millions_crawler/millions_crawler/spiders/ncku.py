8d6b85c07d7314bb1f8c76ece961bacee7fa8cab
NeroHin <hin1997101@gmail.com>
2023-03-09 12:12:28 +0800

    feat: finished to extract page url and title

--- millions_crawler/millions_crawler/spiders/ncku.py
+++ millions_crawler/millions_crawler/spiders/ncku.py
@@ -7,4 +7,23 @@ class NckuSpider(scrapy.Spider):
     start_urls = ["http://www.ncku.edu.tw/"]
 
     def parse(self, response):
-        pass
+        
+        for link in response.css('a::attr(href)').extract():
+            
+            url = response.urljoin(link).replace(' ', '')
+            title = response.css('a[href="'+link+'"]::text').extract_first()
+            
+            if title is None:
+                title = url.split('/')[-1]
+            
+            else:
+                title = title.replace('\n', '').replace('\t\t', '').replace('\t', '')
+                
+                
+            yield {
+                'url': url,
+                'title': title
+            }
+
+        
+    
