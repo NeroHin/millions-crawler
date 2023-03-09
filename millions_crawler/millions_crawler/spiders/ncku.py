3f2c4214538c1b17d4cc9006140146cb5a63c94b
NeroHin <hin1997101@gmail.com>
2023-03-09 12:12:28 +0800

    refactor: spider with oop

--- millions_crawler/millions_crawler/spiders/ncku.py
+++ millions_crawler/millions_crawler/spiders/ncku.py
@@ -1,29 +1,26 @@
 import scrapy
+import re
+from scrapy.linkextractors import LinkExtractor
+from ..items import MillionsCrawlerItem
+
 
 
 class NckuSpider(scrapy.Spider):
     name = "ncku"
     allowed_domains = ["www.ncku.edu.tw"]
     start_urls = ["http://www.ncku.edu.tw/"]
+    pdf_pattern = re.compile(r'.*\.pdf$')
 
     def parse(self, response):
-        
+
+        items = MillionsCrawlerItem()
         for link in response.css('a::attr(href)').extract():
-            
-            url = response.urljoin(link).replace(' ', '')
-            title = response.css('a[href="'+link+'"]::text').extract_first()
-            
-            if title is None:
-                title = url.split('/')[-1]
-            
-            else:
-                title = title.replace('\n', '').replace('\t\t', '').replace('\t', '')
-                
-                
-            yield {
-                'url': url,
-                'title': title
-            }
+
+            items['url'] = response.urljoin(link)
+            items['title'] = response.css('a[href="'+link+'"]::text').extract_first()
+
+
+            yield (items)
 
         
-    
+
