9fc5a9635583dd784ccc9cfad441be4895a87edc
NeroHin <hin1997101@gmail.com>
2023-03-22 01:26:51 +0800

    add: mongod pipeline

--- millions_crawler/millions_crawler/pipelines.py
+++ millions_crawler/millions_crawler/pipelines.py
@@ -11,7 +11,8 @@ from hashlib import md5
 from urllib.parse import unquote
 import re
 import unicodedata as ucd
-
+import pymongo
+from .items import MillionsCrawlerItem
 
 class MillionsCrawlerPipeline:
     def process_item(self, item, spider):
@@ -197,4 +198,32 @@ class WikiPipeline:
         else:
             return item
 
-        # 
+class MongoDBPipeline:
+
+    collection = 'scrapy_items'
+
+    def __init__(self, mongodb_uri, mongodb_db):
+        self.mongodb_uri = mongodb_uri
+        self.mongodb_db = mongodb_db
+        if not self.mongodb_uri: sys.exit("You need to provide a Connection String.")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongodb_uri=crawler.settings.get('MONGODB_URI'),
+            mongodb_db=crawler.settings.get('MONGODB_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongodb_uri)
+        self.db = self.client[self.mongodb_db]
+        # Start with a clean database
+        self.db[self.collection].delete_many({})
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        data = dict(QuoteItem(item))
+        self.db[self.collection].insert_one(data)
+        return item
