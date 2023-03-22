b813236e76a4757784b9d18689a01430d09880c7
NeroHin <hin1997101@gmail.com>
2023-03-22 21:31:28 +0800

    feat: add a env create shell

---
+++ create-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create .env file
+touch .env
+
+# insert content into .env file
+echo "REDIS_ADDRESS=" >> .env
+echo "REDIS_PORT=" >> .env
+echo "REDIS_PASSWORD=" >> .env
+echo "MONGODB_URI=" >> .env
+echo "MONGODB_DB_NAME=" >> .env
+echo "MONGODB_USERNAME=" >> .env
+echo "MONGODB_PASSWORD=" >> .env
