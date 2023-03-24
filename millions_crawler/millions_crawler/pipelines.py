57579e9801b1e521beb5d462bf43ce98a0df78d6
NeroHin <hin1997101@gmail.com>
2023-03-24 19:17:00 +0800

    fix: disable the delete function with db

--- millions_crawler/millions_crawler/pipelines.py
+++ millions_crawler/millions_crawler/pipelines.py
@@ -214,7 +214,7 @@ class MongoDBPipeline:
         self.client = pymongo.MongoClient(self.mongodb_uri)
         self.db = self.client[self.mongodb_db]
         # Start with a clean database
-        self.db[self.collection].delete_many({})
+        # self.db[self.collection].delete_many({})
 
     def close_spider(self, spider):
         self.client.close()
